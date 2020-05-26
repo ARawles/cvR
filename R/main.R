fd4831ada5273b6a33cfa663ae7ef503ab1acb76
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 15:18:27 +0100

    Reknitted

--- R/main.R
+++ R/main.R
@@ -1,4 +1,12 @@
-library("pagedown")
-library("rmarkdown")
+build_commit_push <- function(commit_message = "Test commit") {
+  rmarkdown::render("docs/cv.Rmd")
+  pagedown::chrome_print("docs/cv.html")
+  browser()
+  system(command = paste0("REPO_DIR=", getwd(),
+                          "&& cd ${REPO_DIR} && $ git add --all . && $ git commit -m '",
+                          commit_message,
+                          "' && $ git push master")
+  )
+}
 
-rmarkdown::render("docs/cv.Rmd")
+
