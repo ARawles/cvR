fd07a38ae3d49c73586056f30f8c2902d3b00dab
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 15:22:09 +0100

    test commit

--- R/main.R
+++ R/main.R
@@ -2,9 +2,11 @@ build_commit_push <- function(commit_message = "Test commit") {
   rmarkdown::render("docs/cv.Rmd")
   pagedown::chrome_print("docs/cv.html")
   browser()
-  system(command ="git add --all .")
-  system(command = "git commit -m'test commit'")
-  system(command = "git push master")
+  system(command = 'git config --global user.email "adamrawles@hotmail.co.uk"')
+  system(command = 'git config --global user.name "ARawles"')
+  system(command ='git add --all .')
+  system(command = 'git commit -m"test commit"')
+  system(command = 'git push master')
 #test
 }
 
