630814789212fdf0de38de888e4942030d998680
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 12:29:35 +0100

    Corrected .travis.yml

---
+++ _deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "adamrawles@hotmail.co.uk"
+git config --global user.name "Adam Rawles"
+
+git clone -b wesbite-upload https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../docs/* ./
+  git add --all *
+  git commit -m"Updated CV" || true
+git push -q origin website-upload
