9d51866aa9e472edb1a272ffedad89cb3f4d547c
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 15:21:41 +0100

    Fixed automatic Git integration

--- R/main.R
+++ R/main.R
@@ -2,11 +2,10 @@ build_commit_push <- function(commit_message = "Test commit") {
   rmarkdown::render("docs/cv.Rmd")
   pagedown::chrome_print("docs/cv.html")
   browser()
-  system(command = paste0("REPO_DIR=", getwd(),
-                          "&& cd ${REPO_DIR} && $ git add --all . && $ git commit -m '",
-                          commit_message,
-                          "' && $ git push master")
-  )
+  system(command ="git add --all .")
+  system(command = "git commit -m'test commit'")
+  system(command = "git push master")
+#test
 }
 
 
