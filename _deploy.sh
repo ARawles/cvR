33abea99a47415f5406e932ea18b9e5d7d250ad3
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 14:21:29 +0100

    Fixing onedrive sync issues

--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/sh
 
 set -e
@@ -13,4 +14,21 @@ cd book-output
 cp -r ../docs/* ./
   git add --all *
   git commit -m"Updated CV" || true
+=======
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "adamrawles@hotmail.co.uk"
+git config --global user.name "Adam Rawles"
+
+git clone -b wesbite-upload https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../docs/* ./
+  git add --all *
+  git commit -m"Updated CV" || true
+>>>>>>> parent of 5ada849... typo
 git push -q origin website-upload
