bdcce0e8fb5879e5ccc93e24dcbe65e7ebd7de17
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 15:24:41 +0100

    Finalised automatic build procedure

--- R/main.R
+++ R/main.R
@@ -1,13 +1,12 @@
 build_commit_push <- function(commit_message = "Test commit") {
   rmarkdown::render("docs/cv.Rmd")
   pagedown::chrome_print("docs/cv.html")
-  browser()
+
   system(command = 'git config --global user.email "adamrawles@hotmail.co.uk"')
   system(command = 'git config --global user.name "ARawles"')
   system(command ='git add --all .')
-  system(command = 'git commit -m"test commit"')
-  system(command = 'git push master')
-#test
+  system(command = paste0('git commit -m"', commit_message, '"'))
+  system(command = 'git push origin master')
 }
 
 
