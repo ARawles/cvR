5579775f7f588061283a18f58135a3b3ab18aa5d
ARawles <adamrawles@hotmail.co.uk>
2020-05-26 15:51:11 +0100

    Added automatic generation of a PDF version that doesn't include the PDF link

--- R/main.R
+++ R/main.R
@@ -1,6 +1,14 @@
+create_pdf_version <- function(input_file = "docs/cv.Rmd") {
+  raw_rmd <- readLines(con = "docs/cv.Rmd")
+  pdf_rmd <- gsub('<a href = "https://arawles.co.uk/cv/cv.pdf">PDF Version</a>', replacement = '', x = raw_rmd)
+  writeLines(text = pdf_rmd, con = "docs/cv_pdf.Rmd")
+  closeAllConnections()
+}
+
 build_commit_push <- function(commit_message = "Test commit") {
   rmarkdown::render("docs/cv.Rmd")
-  pagedown::chrome_print("docs/cv.html")
+  create_pdf_version()
+  pagedown::chrome_print("docs/cv_pdf.Rmd")
 
   system(command = 'git config --global user.email "adamrawles@hotmail.co.uk"')
   system(command = 'git config --global user.name "ARawles"')
@@ -8,5 +16,8 @@ build_commit_push <- function(commit_message = "Test commit") {
   system(command = paste0('git commit -m"', commit_message, '"'))
   system(command = 'git push origin master')
 }
+
+
+
 
 
